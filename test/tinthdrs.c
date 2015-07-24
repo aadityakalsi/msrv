876cecfaef801b7b593b9d33ac1f15893250bfce
Aaditya Kalsi <akalsi.dev@gmail.com>
2015-07-23 23:39:07 -0400

    Adding inttypes and stdint headers

---
+++ test/tinthdrs.c
@@ -0,0 +1,14 @@
+/**
+ * \file talloc.c
+ * \date 2015
+ */
+
+#include "unittest.h"
+
+/* TEST includes
+ */
+#include <msrv/msrv_stdint.h>
+#include <msrv/msrv_inttypes.h>
+
+void SetupTests(void)
+{ }
